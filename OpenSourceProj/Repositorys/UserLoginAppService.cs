ba090df17fa2680bb2ff95bf2a1cfc3a7e4d06dd
Sandeep Arukolu <sandeeplns@outlook.com>
2024-03-20 23:39:23 +0530

    Change Removed in Service

--- OpenSourceProj/Repositorys/UserLoginAppService.cs
+++ OpenSourceProj/Repositorys/UserLoginAppService.cs
@@ -18,7 +18,6 @@ namespace OpenSourceProj.Repositorys
         public UserInfo GetLoginInfo(LoginModal loginModal)
         {         
          var list = _dbcontext.Users.Where(x => x.Email == loginModal.Email && x.Password == loginModal.Password).FirstOrDefault();
-            string dd = "";
             return  list;
         }
 
