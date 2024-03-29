892c29ec67cdb3a666979f9da42f147d9059a0b2
Sandeep Arukolu <sandeeplns@outlook.com>
2024-03-29 20:13:16 +0530

    Jwt Token implemented

--- OpenSourceProj/Controllers/UserLoginOperation.cs
+++ OpenSourceProj/Controllers/UserLoginOperation.cs
@@ -12,21 +12,28 @@ namespace OpenSourceProj.Controllers
     public class UserLoginOperation : ControllerBase
     {
         private readonly IUserLoginAppService _userRepostiory;
-
-        public UserLoginOperation(IUserLoginAppService userRepostiory)
+        private readonly IJwtService _jwtService;
+        public UserLoginOperation(IUserLoginAppService userRepostiory, IJwtService jwtService)
         {
             _userRepostiory = userRepostiory;
+            _jwtService = jwtService;
         }
 
+       
         [HttpPost]
         [Route("UserLogin")]
         public IActionResult Login(LoginModal loginModal)
         {
           var response=  _userRepostiory.GetLoginInfo(loginModal);
-            return Ok(response);
+            if (response != null)
+            {
+               string Token = _jwtService.GenerateToken(response);
+                return Ok(Token);
+            }
+            return Ok();
         }
         [HttpPost]
-        [Route("UserRegister")]
+        [Route("UserSignIn")]
         public async Task<IActionResult> register(UserInfo userinfo)
         {
           var response =  _userRepostiory.SaveUser(userinfo);
